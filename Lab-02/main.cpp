46cf48bb747a740b3d577a0cf97266be92003b35
Alexander <alik.alfier@gmail.com>
2026-04-04 17:03:36 +0300

    Update main.cpp

--- Lab-02/main.cpp
+++ Lab-02/main.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+	
+	long long n;
+	cout << "Enter number n: ";
+	cin >> n;
+
+	int count = 0;
+
+	while (n > 0)
+	{
+		int digit = n % 10;
+
+		if (digit == 1)
+		{
+			count++;
+
+			if (count == 6)
+			{
+				break;
+			}
+		}
+		else
+		{
+			count = 0;
+		}
+
+		n /= 10;
+	}
+
+	if (count == 6)
+		cout << "Result: Yes" << endl;
+	else
+		cout << "Result: No" << endl;
+
+	return 0;
+}
